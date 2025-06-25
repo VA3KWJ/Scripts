bb766eb7ccf49dbe884ae61a304767c882d42227
Stu <stu.waller@pm.me>
2025-06-25 07:55:14 -0400

    Fix user creation script and port check logic

--- ConnectionPortCheck.ps1
+++ ConnectionPortCheck.ps1
@@ -1,13 +1,13 @@
-﻿# Set the tartget computer (Hostname or IP)
+﻿# Set the target computer (hostname or IP)
 $computer = "TargetHostOrIP"
 
-# Define the port rante
+# Define the port range
 $ports = 79..81
 
 # Set the log path
 $logFile = "C:\Temp\PortConnLog.txt"
 
-# Init time for last stable
+# Time of last summary log
 $lastSummaryTime = Get-Date
 
 # Main Program
@@ -16,22 +16,21 @@ while ($true) {
     $allPortsStable = $true
 
     foreach ($port in $ports) {
-        # Run Test-NetConnection for the current prot
+        # Run Test-NetConnection for the current port
         $result = Test-NetConnection -ComputerName $computer -Port $port -WarningAction SilentlyContinue
         $timeStamp = $currentTime.ToString("yyyy-MM-dd HH:mm:ss")
 
         if ($result.TcpTestSucceeded) {
-            # Log a success for port
+            # Log a success for the port
             $logMessage = "$timeStamp - Connection to $computer on $port succeeded."
             Add-Content -Path $logFile -Value $logMessage
-            }
-
-        else {
-            # Log failure
+        } else {
+            # Log failure and mark ports unstable
             $logMessage = "$timeStamp - Connection to $computer on port $port has failed."
             Add-Content -Path $logFile -Value $logMessage
-            }
+            $allPortsStable = $false
         }
+    }
 
 # If all ports are stabe and 5 min has elapsed, log a message
 if ($allPortsStable -and (($currentTime - $lastSummaryTime).TotalMinutes -ge 5)) {
